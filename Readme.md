d117144b1014985bcfca12a41adba0641d525f88
debuff <arctan90@163.com>
2024-03-12 17:59:48 +0800

    First commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# create env
+```
+# create conda env
+conda create -n nvlet python=3.11
+# activate it
+conda activate  nvlet
+# install torch
+pip install torch
+# or on mac pip3 install --pre torch torchaudio --index-url https://download.pytorch.org/whl/nightly/cpu
+```
---
+++ scanner/GpuScaner.py
@@ -0,0 +1,34 @@
+import threading
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+import torch
+
+
+class GpuScaner:
+    def __init__(self):
+        self.scheduler = BlockingScheduler()
+
+    def start(self):
+        self.scheduler.add_job(_job, 'interval', seconds=20)
+
+        thread = threading.Thread(target=self.scheduler.start())
+        thread.daemon = True
+        thread.start()
+
+
+# key-gpuId value-busy:True
+gpu_busy_map = []
+
+
+def _job():
+    global gpu_busy_map
+    print("Getting GPU information...")
+    try:
+        gpu_ids = torch.cuda.device_count()
+
+        for gpu_id in range(gpu_ids):
+            current_stream = torch.cuda.current_stream(gpu_id)
+            gpu_busy_map[gpu_id] = not current_stream.is_waiting()
+        print(gpu_busy_map)
+    except Exception as e:
+        print(f"Error: {e}")
