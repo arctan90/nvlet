4a28b134c84dc363dac608c2d0c11e3adb290477
debuff <arctan90@163.com>
2024-03-13 10:41:39 +0800

    change gpu scanner from torch to nvidia-smi

--- scanner/GpuScaner.py
+++ scanner/GpuScaner.py
@@ -1,7 +1,7 @@
 import threading
+import subprocess
 
 from apscheduler.schedulers.blocking import BlockingScheduler
-import torch
 
 
 class GpuScaner:
@@ -10,25 +10,27 @@ class GpuScaner:
 
     def start(self):
         self.scheduler.add_job(_job, 'interval', seconds=20)
-
         thread = threading.Thread(target=self.scheduler.start())
         thread.daemon = True
         thread.start()
 
 
 # key-gpuId value-busy:True
-gpu_busy_map = {}
+gpu_busy_map_smi = {}
 
 
 def _job():
-    global gpu_busy_map
-    print("Getting GPU information...")
     try:
-        gpu_ids = torch.cuda.device_count()
+        # invoke nvidia-smi
+        result = subprocess.check_output(['nvidia-smi', '--query-gpu=utilization.gpu', '--format=csv,noheader,nounits'])
 
-        for gpu_id in range(gpu_ids):
-            current_stream = torch.cuda.current_stream(gpu_id)
-            gpu_busy_map[gpu_id] = not current_stream.is_waiting()
-        print(gpu_busy_map)
+        # decode
+        gpu_utilization = [float(utilization) for utilization in result.decode('utf-8').strip().split('\n')]
+
+        for gpu_id, utilization in enumerate(gpu_utilization):
+            gpu_busy_map_smi[gpu_id] = utilization > 10.0
+
+        print(gpu_busy_map_smi)
+
     except Exception as e:
         print(f"Error: {e}")
